ab029ebd4bdacb31f86d233a4eae1f8881b32e14
mandaltapesh <tapesh.mandal@hotmail.com>
2020-04-23 20:15:32 +0530

    Add main function and change to hex values

--- firmpy.py
+++ firmpy.py
@@ -1,18 +1,24 @@
 import pyfirmata
 import time
 
-ON = 1
-OFF = 0
+ON = 0x1
+OFF = 0x0
 PIN = 8
 # delay in seconds
-DELAY = 4 
+DELAY = 8
 
 board = pyfirmata.Arduino('/dev/ttyACM0')
 
 
-while True:
-    board.digital[PIN].write(ON)
-    time.sleep(DELAY)
-    board.digital[PIN].write(OFF)
-    time.sleep(DELAY)
+def main():
+    """Main function"""
 
+    while True:
+        board.digital[PIN].write(ON)
+        time.sleep(DELAY)
+        board.digital[PIN].write(OFF)
+        time.sleep(DELAY)
+
+
+if __name__ == "__main__":
+    main()
