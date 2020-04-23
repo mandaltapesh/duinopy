d34f49ccc3d78a9aea8d386fb6cf4300c92e31e2
mandaltapesh <tapesh.mandal@hotmail.com>
2020-04-23 21:42:33 +0530

    Add functions, logging etc.

--- firmpy.py
+++ firmpy.py
@@ -1,23 +1,46 @@
+import logging
 import pyfirmata
 import time
 
-ON = 0x1
-OFF = 0x0
+ON = 1
+OFF = 0
 PIN = 8
 # delay in seconds
-DELAY = 8
+DELAY = 10
 
 board = pyfirmata.Arduino('/dev/ttyACM0')
+board.digital[PIN].mode = pyfirmata.OUTPUT
+
+
+def for_delay(delay_time):
+    """
+    Function to delay/wait for a certain time interval.
+    :param delay_time: int, in secs
+    :return: None
+    """
+    logging.warning(msg="In Delay function")
+    time.sleep(delay_time)
+
+
+def set_pin_high_low(pin, value):
+    """
+    Function to set pin to high.
+    :param pin: int
+    :param value: hex
+    :return: None
+    """
+    logging.warning(msg="In set_pin_high_low function. value = {}".format(value))
+    board.digital[pin].write(value)
 
 
 def main():
     """Main function"""
 
     while True:
-        board.digital[PIN].write(ON)
-        time.sleep(DELAY)
-        board.digital[PIN].write(OFF)
-        time.sleep(DELAY)
+        set_pin_high_low(PIN, ON)
+        for_delay(DELAY)
+        set_pin_high_low(PIN, OFF)
+        for_delay(DELAY)
 
 
 if __name__ == "__main__":
