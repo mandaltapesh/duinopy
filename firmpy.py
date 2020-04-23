61ae5edc179bbd063e294f8850e5f4d735950481
mandaltapesh <tapesh.mandal@hotmail.com>
2020-04-23 23:00:40 +0530

    Add menu

--- firmpy.py
+++ firmpy.py
@@ -8,7 +8,7 @@ PIN = 8
 # delay in seconds
 DELAY = 10
 
-board = pyfirmata.Arduino('/dev/ttyACM0')
+board = pyfirmata.Arduino('/dev/ttyACM2')
 board.digital[PIN].mode = pyfirmata.OUTPUT
 
 
@@ -29,18 +29,27 @@ def set_pin_high_low(pin, value):
     :param value: hex
     :return: None
     """
+    # import pdb;
+    # pdb.set_trace()
+
     logging.warning(msg="In set_pin_high_low function. value = {}".format(value))
     board.digital[pin].write(value)
 
-
+    
 def main():
     """Main function"""
 
     while True:
-        set_pin_high_low(PIN, ON)
-        for_delay(DELAY)
-        set_pin_high_low(PIN, OFF)
-        for_delay(DELAY)
+        choice = int(input("Enter a choice \n 1. Turn LED on [1] \n 2. Turn LED off \n 3. Quit [3] \n >> "))
+        if choice == 1:
+            set_pin_high_low(PIN, ON)
+            for_delay(DELAY)
+        elif choice == 2:
+            set_pin_high_low(PIN, OFF)
+            for_delay(DELAY)
+        else:
+            board.exit()
+            break
 
 
 if __name__ == "__main__":
